bb4ed9bfac0875a0344f448dc362a278df32da7c
John Qi <john.qi@interfaceware.com>
2022-06-06 20:36:07 +0000

    Added SVR prefix to local functions

--- server.lua
+++ server.lua
@@ -1,10 +1,10 @@
-local Connections = {}
-local Running = false
-local Debug = false
-local IdleTimeout = 60
-local OnAccept, OnData
+local SVRconnections = {}
+local SVRrunning = false
+local SVRdebug = false
+local SVRidleTimeout = 60
+local SVRonAccept, SVRonData
 
-local function create_connection(Id, Address)
+local function SVRcreateConnection(Id, Address)
    local meta = {
       id = Id,
       ip = Address,
@@ -19,7 +19,7 @@ local function create_connection(Id, Address)
    return connection
 end
 
-local function socket_connection(Connection)
+local function SVRsocketConnection(Connection)
    if type(Connection) ~= "table" then
       return false
    end
@@ -33,18 +33,18 @@ local function socket_connection(Connection)
    return true
 end
 
-local function touch_connection(Connection)
-   if not socket_connection(Connection) then
+local function SVRtouchConnection(Connection)
+   if not SVRsocketConnection(Connection) then
       erorr("input is not a socket connection")
    end
    getmetatable(Connection).ts = os.time()
 end
 
-local function purge_idle_connections()
+local function SVRpurgeIdleConnections()
    local now = os.time()
-   for id, connection in pairs(Connections) do
-      if now - getmetatable(connection).ts > IdleTimeout then
-         if Debug then
+   for id, connection in pairs(SVRconnections) do
+      if now - getmetatable(connection).ts > SVRidleTimeout then
+         if SVRdebug then
             iguana.logDebug("Closing idle connection " .. tostring(Id) .. " from " .. Address)
          end
          socket.close_a(id)
@@ -53,39 +53,39 @@ local function purge_idle_connections()
 end
 
 socket.onAccept = function(Address, Id)
-   if Debug then
+   if SVRdebug then
       iguana.logInfo("Established new connection " .. tostring(Id) .. " from " .. Address)
    end
-   if Connections[Id] then
+   if SVRconnections[Id] then
       error("New connection has the same Id as an existing connection")
    end
-   Connections[Id] = create_connection(Id, Address)
-   if not OnAccept then
+   SVRconnections[Id] = SVRcreateConnection(Id, Address)
+   if not SVRonAccept then
       error("Accept callback is not set")
    end
-   OnAccept(Connections[Id])
-   purge_idle_connections()
+   SVRonAccept(SVRconnections[Id])
+   SVRpurgeIdleConnections()
 end
 
 socket.onData = function(Data, Id)
-   if Debug then
+   if SVRdebug then
       iguana.logDebug("Received data from connection " .. tostring(Id) .. "\n")
    end
-   if not Connections[Id] then
+   if not SVRconnections[Id] then
       error("Connection not found")
    end
-   touch_connection(Connections[Id])
-   OnData(Connections[Id], Data)
-   purge_idle_connections()
+   SVRtouchConnection(SVRconnections[Id])
+   SVRonData(SVRconnections[Id], Data)
+   SVRpurgeIdleConnections()
 end
 
 socket.onWrite = function(Id)
-   if Debug then
+   if SVRdebug then
       iguana.logDebug("Connection " .. tostring(Id) .. " is ready to receive more data")
    end
-   touch_connection(Connections[Id])
+   SVRtouchConnection(SVRconnections[Id])
 	-- TODO: callback for streaming data
-   purge_idle_connections()
+   SVRpurgeIdleConnections()
 end
 
 socket.onClose = function(Data, Id)
@@ -93,89 +93,88 @@ socket.onClose = function(Data, Id)
    if #Data > 0 then
       log_message = log_message .. " with non-empty buffer: " .. Data:sub(1, 1024)
    end
-   if Debug then
+   if SVRdebug then
       iguana.logInfo(log_message)
    end
-   Connections[Id] = nil
-   purge_idle_connections()
+   SVRconnections[Id] = nil
+   SVRpurgeIdleConnections()
 end
 
 socket.onError = function(Data, Id)
-   if Debug then
+   if SVRdebug then
       iguana.logError("Connection " .. tostring(Id) .. " encountered error: " .. Data)
    end
    -- TODO: callback
-   purge_idle_connections()
+   SVRpurgeIdleConnections()
 end
 
 
-local function start_server(Config)
-   if Running then
+local function SVRstartServer(Config)
+   if SVRrunning then
       error("Server already running")
    end
    socket.listen_a(Config)
-   Running = true
+   SVRrunning = true
 end
 
-local function stop_server()
-   if not Running then
+local function SVRstopServer()
+   if not SVRrunning then
       return
    end
    -- close all connections and stop listening
    -- wipe callbacks?
-   Running = false
+   SVRrunning = false
 end
 
-local function send_response(Connection, Data)
-   if not socket_connection(Connection) then
+local function SVRsendResponse(Connection, Data)
+   if not SVRsocketConnection(Connection) then
       erorr("input is not a socket connection")
    end
    socket.send_a(Data, getmetatable(Connection).id)
 end
 
-local function close_connection(Connection)
-   if not socket_connection(Connection) then
+local function SVRcloseConnection(Connection)
+   if not SVRsocketConnection(Connection) then
       erorr("input is not a socket connection")
    end
    socket.close_a(getmetatable(Connection).id)
 end
 
-local function set_debug_mode(State)
+local function SVRsetDebugMode(State)
    if type(State) ~= "boolean" then
       error("must be a bool")
    end
-   Debug = State
+   SVRdebug = State
 end
 
-local function set_idle_timeout(Seconds)
+local function SVRsetIdleTimeout(Seconds)
    if type(Seconds) ~= "number" then
       error("must be an integer")
    end
-   IdleTimeout = Seconds
+   SVRidleTimeout = Seconds
 end
 
-local function set_accept_callback(Callback)
+local function SVRsetAcceptCallback(Callback)
    if type(Callback) ~= "function" then
       error("must be a function")
    end
-   OnAccept = Callback
+   SVRonAccept = Callback
 end
 
-local function set_Data_callabck(Callback)
+local function SVRsetDataCallabck(Callback)
    if type(Callback) ~= "function" then
       error("must be a function")
    end
-   OnData = Callback
+   SVRonData = Callback
 end
 
 return {
-   start       = start_server,
-   stop        = stop_server,
-   respond     = send_response,
-   close       = close_connection,
-   setDebug    = set_debug_mode,
-   setTimeout  = set_idle_timeout,
-   setOnAccept = set_accept_callback,
-   setOnData   = set_Data_callabck,
-   create_connection = create_connection
+   start       = SVRstartServer,
+   stop        = SVRstopServer,
+   respond     = SVRsendResponse,
+   close       = SVRcloseConnection,
+   setDebug    = SVRsetDebugMode,
+   setTimeout  = SVRsetIdleTimeout,
+   setOnAccept = SVRsetAcceptCallback,
+   setOnData   = SVRsetDataCallabck
 }
