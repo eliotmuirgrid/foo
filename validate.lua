b99f1ab8527bbcbe669cd8716a3e8b9e4dac52bb
John Qi <john.qi@interfaceware.com>
2022-06-10 15:45:01 -0400

    Updated validate to use CHK library

--- validate.lua
+++ validate.lua
@@ -1,18 +1,6 @@
-local Configs = component.fields()
-local CHKerrMsg
+require "CHK.CHKcustomField"
 
-local function CHKreportError()
-   if CHKerrMsg then
-      component.setStatus{data = '<p style="color:red;">' .. CHKerrMsg .. '</p>'}
-      error(CHKerrMsg, 3)
-   end
-end
-
--- Port
-if not Configs.Port then
-   CHKerrMsg = "Required custom field 'Port' not found."
-end
-CHKreportError()
+CHKcustomField("Port", "port")
 
 -- all good, reset status
 component.setStatus{data = ""}
