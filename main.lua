7fc30e8c1b06d6186ba539cef77cd1918573ef25
Eliot Muir <eliot.muir@icloud.com>
2022-06-15 13:41:41 -0400

    Changing to queue.

--- main.lua
+++ main.lua
@@ -5,8 +5,8 @@ require "LLP.LLPserver"
 -- The main function is called when a message is received by the server,
 -- and an ACK message must be returned when it exits
 function main(Message)
-   -- log the message
-   iguana.logMessage(Message)
+   -- Queue the message
+   queue.push{data=Message}
    -- return ACK
    return LLPgenerateAck(Message)
 end
