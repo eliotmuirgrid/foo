449cad42909e0f09d3de2f6b1443ad039bc5585b
Eliot Muir <eliot.muir@icloud.com>
2022-10-12 11:27:55 -0400

    Removed stuff from main that was unnecessary comments.

--- main.lua
+++ main.lua
@@ -1,12 +1,10 @@
 require "LLPS.LLPSserver"
 require "LLPS.LLPSack"
+
 -- The main function is called when a message is received by the server,
 -- and an ACK message must be returned when it exits
 function main(Message)
-   start()
-   -- Queue the message
    queue.push{data=Message}
-   -- return ACK
    return LLPgenerateAck(Message)
 end
 
