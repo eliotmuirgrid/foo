a8398ab953235a69568bac449404fca3d694e5c1
Eliot Muir <eliot.muir@interfaceware.com>
2023-10-11 11:44:54 -0400

    Simplified generation of ACKs to use vmd file.

--- main.lua
+++ main.lua
@@ -1,16 +1,31 @@
 require "LLPS.LLPSserver"
-require "LLPS.LLPSack"
 
--- The main function is called when a message is received by the server,
--- and an ACK message must be returned when it exits
+-- CUSTOMIZE this ACK generation if it doesn't meet your requirements 
+function GenerateAck(Message)
+   local M = hl7.parse{vmd="ack.vmd", data=Message}
+   local A = hl7.message{vmd="ack.vmd", name="ACK"}
+   A.MSH[3][1] = M.MSH[5][1]
+   A.MSH[4][1] = M.MSH[6][1]
+   A.MSH[5][1] = M.MSH[3][1]
+   A.MSH[6][1] = M.MSH[4][1]
+   A.MSH[7]    = M.MSH[7]
+   A.MSH[8]    = M.MSH[8]
+   A.MSH[9][1] = 'ACK'
+   A.MSH[10]   = 'A'..M.MSH[10]
+   A.MSH[11]   = M.MSH[11]
+   A.MSH[12]   = M.MSH[12]
+   A.MSA[1]    = "AA"
+   A.MSA[2]    = M.MSH[10] 
+   return A:S()
+end
+
+-- The main function is called when a message is received by the server
+-- An ACKnowledgement message must be returned when it exits
 function main(Message)
    queue.push{data=Message}
-   return LLPgenerateAck(Message)
+   return GenerateAck(Message)
 end
 
-function start()
-   local Config = component.fields()
-   socket.listen_a{port=Config.Port}
-end
 
-start()
+--This function starts the LLP server.
+LLPstart()
